e7f5ba5121605b9b3a734616754d202c27fa124c
skiblue <skiblue@pop-os>
2021-04-13 00:03:30 +0530

    added styles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-const theHobbit2 = new Book('The Hobbit2', 'J.R.R. Tolkien', 295, false);
+const theHobbit2 = new Book('The Hobbit2', 'J.R.R. Tolkien', 295, true);
 
 let myLibrary = [theHobbit, theHobbit2, theHobbit, theHobbit, theHobbit];  // array to store books
 
@@ -21,7 +21,9 @@ function displayBooks() {
 
     let rows = (myLibrary.length % 4 == 0) ? myLibrary.length/4 : Math.floor(myLibrary.length / 4) + 1;
     if (rows == 0) {
-        main.style.display = 'block';
+        main.style.display = 'none';
+    } else {
+        main.style.display = 'grid';
     }
     for (let i = 0; i < myLibrary.length; i++) {
         const div = document.createElement('div');
@@ -29,9 +31,30 @@ function displayBooks() {
         div.setAttribute('id', i);
         const ul = document.createElement('ul');
 
-        for (let key in myLibrary[i]) {
+        for (let key in myLibrary[i]) { 
             const li = document.createElement('li');
-            li.innerText = myLibrary[i][key];
+            li.classList.add(key);
+            if (key == 'read') {
+                const label = document.createElement('label');
+                label.setAttribute('for', 'read');
+                label.innerText = 'Read: ';
+                li.appendChild(label);
+
+                const checkbox = document.createElement('input');
+                checkbox.setAttribute('type', 'checkbox');
+                checkbox.setAttribute('data-key', i);
+                checkbox.classList.add('read-check');
+                checkbox.checked = myLibrary[i][key];
+                li.appendChild(checkbox);
+
+                li.classList.add('read-div');
+                ul.appendChild(li);
+                continue;
+            } else if (key == 'pages') {
+                li.innerText = `Pages: ${myLibrary[i][key]}`;
+            } else {
+                li.innerText = myLibrary[i][key];
+            }
             ul.appendChild(li);
         }
         const button = document.createElement('button');
@@ -43,19 +66,29 @@ function displayBooks() {
         main.appendChild(div);
     }
     main.style.gridTemplateColumns = `repeat(4, 1fr)`;
-    main.style.gridTemplateRows = `repeat(${rows}, 100px)`;
+    main.style.gridTemplateRows = `repeat(${rows}, 210px)`;
 
     document.querySelectorAll('.delete').forEach(del => {
         del.addEventListener('click', () => {
+            let flag = confirm("Are you sure you want to delete this?");
+            if (!flag)
+                return;
             const index = +del.getAttribute('data-key');
             myLibrary.splice(index, 1);
             displayBooks();
         });
     });
+
+    document.querySelectorAll('.read-check').forEach(check => {
+        check.addEventListener('click', () => {
+            const index = check.getAttribute('data-key');
+            myLibrary[index]['read'] = check.checked;
+        });
+    });
 }
 
 document.querySelector('#newBook').addEventListener('click', () => {
-    document.querySelector('.form').classList.add('show-form');
+    document.querySelector('.form').classList.add('show-form');2
     const form = document.querySelector('form');
     form.reset();
 });
